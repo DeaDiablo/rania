cab9657898bb649821884b6280e5fb662f96e469
Tim <dudkinta@yandex.ru>
2013-05-31 18:16:21 +0400

    Add 2 file

---
+++ src/com/android/game/rania/model/element/Locations.java
@@ -0,0 +1,9 @@
+package com.android.game.rania.model.element;
+
+public class Locations {
+	public int id;
+	public int StarType;
+	public int x;
+	public int y;
+	public String StarName;
+}
---
+++ src/com/android/game/rania/model/element/Planets.java
@@ -0,0 +1,10 @@
+package com.android.game.rania.model.element;
+
+public class Planets {
+	public int id;
+	public int PlanetType;
+	public int R_speed;
+	public int Orbit;
+	public int Radius;
+	public String PlanetName;
+}
